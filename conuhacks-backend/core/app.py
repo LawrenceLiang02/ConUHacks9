3a5034af23c9b40b83ed555dc1a62b123235043a
Ludovic Marcoux Hoyos <ludomarcoux098@gmail.com>
2025-02-01 14:37:19 -0500

    API recipes call

--- conuhacks-backend/core/app.py
+++ conuhacks-backend/core/app.py
@@ -1,10 +1,70 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+API_KEY = os.getenv('API_KEY')  # Using the key from .env
 
 @app.route('/')
 def home():
     return "Hello, Flask!"
+
+
+@app.route('/recipes/getRecipesFromIngredients', methods=['GET'])
+def get_recipes_from_ingredients():
+    try:
+        ingredients = request.args.get('ingredients', '')  # Get ingredients from query params
+        if not ingredients:
+            return jsonify({'error': 'No ingredients provided'}), 400
+
+        url = 'https://api.spoonacular.com/recipes/findByIngredients'
+        params = {
+            'apiKey': API_KEY,
+            'ingredients': ingredients,
+            'number': 3,
+            'ranking': 1,
+            'ignorePantry': True
+        }
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        return jsonify(response.json())
+    except requests.exceptions.RequestException as e:
+        return jsonify({'error': str(e)}), 500
+    
+
+@app.route('/recipes/getRecipe', methods=['GET'])
+def get_recipe():
+    try:
+        recipe = request.args.get('recipe', '')
+        cuisine = request.args.get('cuisine', '')
+        diet = request.args.get('diet', '')
+        intolerances = request.args.get('intolerances', '').split(',')
+        recipe_type = request.args.get('recipe_type', '')
+        
+        url = 'https://api.spoonacular.com/recipes/complexSearch'
+        params = {
+            'apiKey': API_KEY,
+            'query': recipe,
+            'cuisine': cuisine,
+            'diet': diet,
+            'intolerances': ','.join(intolerances),
+            'type': recipe_type,
+            'instructionsRequired': True,
+            'fillIngredients': True,
+            'addRecipeInformation': True,
+            'addRecipeInstructions': True,
+            'addRecipeNutrition': True,
+            'number': 3
+        }
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        return jsonify(response.json())
+    except requests.exceptions.RequestException as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
